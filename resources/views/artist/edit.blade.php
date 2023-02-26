f4954a94005f018bd3f0acce9085c61f08f8fb6f
Ismail T <takkalismael@gmail.com>
2023-02-26 18:55:12 +0100

    Breeze correction regression

--- resources/views/artist/edit.blade.php
+++ resources/views/artist/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('title', 'Modifier les données de l\'artiste')
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ArtistController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/artist',[ArtistController::class, 'index'])->name('artist.index');
 Route::get('/artist/{id}',[ArtistController::class, 'show'])->where('id','[0-9]+')->name('artist.show');
 Route::get('/artist/edit/{id}',[ArtistController::class, 'edit'])->where('id','[0-9]+')->name('artist.edit');
@@ -25,3 +35,6 @@ Route::put('/artist/{id}',[ArtistController::class, 'update'])->where('id','[0-9
 Route::get('/artist/create', [ArtistController::class, 'create'])->name('artist.create');
 Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
 Route::delete('/artist/{id}', [ArtistController::class, 'destroy']) ->where('id', '[0-9]+')->name('artist.delete');
+
+require __DIR__.'/auth.php';
+
